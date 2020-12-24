0da6d3bf0420ef69dfd4b3d1c900e48df3afa7db
devanych <mail@devanych.ru>
2020-12-24 12:57:29 +0300

    Refactoring SyslogTarget

--- src/SyslogTarget.php
+++ src/SyslogTarget.php
@@ -9,36 +9,50 @@ use RuntimeException;
 use Yiisoft\Log\Message;
 use Yiisoft\Log\Target;
 
+use const LOG_ALERT;
+use const LOG_CRIT;
+use const LOG_DEBUG;
+use const LOG_EMERG;
+use const LOG_ERR;
+use const LOG_INFO;
+use const LOG_NOTICE;
+use const LOG_ODELAY;
+use const LOG_PID;
+use const LOG_USER;
+use const LOG_WARNING;
+
 /**
  * SyslogTarget writes log to syslog.
  */
-class SyslogTarget extends Target
+final class SyslogTarget extends Target
 {
     /**
-     * @var string syslog identity
+     * @var string The openlog identity. This is a bitfield passed as the `$ident` parameter to `openlog()`.
+     *
+     * @see https://www.php.net/openlog
      */
     private string $identity;
 
     /**
-     * @var int syslog facility.
-     */
-    private int $facility = LOG_USER;
-
-    /**
-     * @var int openlog options. This is a bitfield passed as the `$option` parameter to [openlog()](http://php.net/openlog).
+     * @var int The openlog options. This is a bitfield passed as the `$option` parameter to `openlog()`.
+     *
      * Defaults to `LOG_ODELAY | LOG_PID`.
      *
-     * @see http://php.net/openlog for available options.
+     * @see https://www.php.net/openlog
      */
-    private int $options = LOG_ODELAY | LOG_PID;
+    private int $options;
 
     /**
-     * @var bool Whether the message format was previously set.
+     * @var int The openlog facility. his is a bitfield passed as the `$facility` parameter to `openlog()`.
+     *
+     * Defaults to `LOG_USER`.
+     *
+     * @see https://www.php.net/openlog
      */
-    private bool $isMessageFormatSet = false;
+    private int $facility;
 
     /**
-     * @var array syslog levels
+     * @var array The syslog levels.
      */
     private array $syslogLevels = [
         LogLevel::EMERGENCY => LOG_EMERG,
@@ -51,46 +65,36 @@ class SyslogTarget extends Target
         LogLevel::DEBUG => LOG_DEBUG,
     ];
 
-    public function setIdentity(string $identity): self
+    /**
+     * @param string $identity The openlog identity. This is a bitfield passed as the `$ident` parameter to `openlog()`.
+     * @param int $options The openlog options. This is a bitfield passed as the `$option` parameter to `openlog()`.
+     * @param int $facility The openlog facility. his is a bitfield passed as the `$facility` parameter to `openlog()`.
+     */
+    public function __construct(string $identity, int $options = LOG_ODELAY | LOG_PID, int $facility = LOG_USER)
     {
         $this->identity = $identity;
-        return $this;
-    }
-
-    public function setFacility(int $facility): self
-    {
-        $this->facility = $facility;
-        return $this;
-    }
-
-    public function setOptions(int $options): self
-    {
         $this->options = $options;
-        return $this;
-    }
+        $this->facility = $facility;
+        parent::__construct();
 
-    public function setFormat(callable $format): Target
-    {
-        $this->isMessageFormatSet = true;
-        return parent::setFormat($format);
+        $this->setFormat(static function (Message $message) {
+            return "[{$message->level()}][{$message->context('category', '')}] {$message->message()}";
+        });
     }
 
     /**
      * Writes log messages to syslog.
-     * Starting from version 2.0.14, this method throws RuntimeException in case the log can not be exported.
      *
-     * @throws RuntimeException
+     * @see https://www.php.net/openlog
+     * @see https://www.php.net/syslog
+     * @see https://www.php.net/closelog
+     *
+     * @throws RuntimeException If unable to export log through system log.
      */
-    public function export(): void
+    protected function export(): void
     {
         $formattedMessages = $this->getFormattedMessages();
         openlog($this->identity, $this->options, $this->facility);
-
-        if (!$this->isMessageFormatSet) {
-            $this->setFormat(static function (Message $message) {
-                return "[{$message->level()}][{$message->context('category', '')}] {$message->message()}";
-            });
-        }
 
         foreach ($this->getMessages() as $key => $message) {
             if (syslog($this->syslogLevels[$message->level()], $formattedMessages[$key]) === false) {
