aeae865d7cf9eb6a7cc014b378c2990c27ea0c85
Максим Спирков <spirkov.2001@mail.ru>
2026-01-27 18:54:20 +0400

    fix CS

--- config/di.php
+++ config/di.php
@@ -7,7 +7,7 @@ use Yiisoft\Log\Target\Syslog\SyslogTarget;
 /** @var array $params */
 
 return [
-    SyslogTarget::class => static fn () => new SyslogTarget(
+    SyslogTarget::class => static fn() => new SyslogTarget(
         identity: $params['yiisoft/log-target-syslog']['syslogTarget']['identity'],
         levels: $params['yiisoft/log-target-syslog']['syslogTarget']['levels'] ?? [],
     ),
